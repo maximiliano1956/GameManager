4b727f608c96c5e51294720ccd8d13021b80d9d5
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2020-01-27 11:35:52 +0100

    Update CrossCompiling.sh

--- GameManager/UciClient/CrossCompiling.sh
+++ GameManager/UciClient/CrossCompiling.sh
@@ -1,12 +1,4 @@
 set -x
-cd ../MyChess
-rm -f *.o *.a *.dll
-x86_64-w64-mingw32-gcc -I../GameManager -D_LIB -c *.c
-ar rcs libmychess.a *.o
-cd ../GameManager
-rm -f *.o *.a *.dll
-x86_64-w64-mingw32-gcc -c -D_LIB *.c
-ar rcs libgamemanager.a *.o
-cd ../UciClient
-rm -f *.o UciClient.exe
-x86_64-w64-mingw32-gcc -L../GameManager -L../MyChess *.c -lgamemanager -lmychess -lws2_32 -o UciClient.exe
+rm -f UciClient.exe 
+x86_64-w64-mingw32-gcc -I../GameManager -D_LIB ../GameManager/*.c ../MyChess/*.c *.c -L. -lws2_32 -o UciClient.exe
+strip UciClient.ex
