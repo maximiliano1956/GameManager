3a0a357f257e7d8b10ab5e27bdd4ee1007dcd59c
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2020-01-27 11:45:41 +0100

    Update CrossCompilingDll.sh

--- GameManager/CrossCompilingDll.sh
+++ GameManager/CrossCompilingDll.sh
@@ -1,7 +1,7 @@
 set -x
 cd GameManager
-rm -f *.o *.a *.dll
+rm -f GameManager.dll
 i686-w64-mingw32-gcc -shared -municode *.c GameManager_dll.def -o GameManager.dll
 cd ../MyChess
-rm -f *.o *.a *.dll
+rm -f MyChess.dll
 i686-w64-mingw32-gcc -I../GameManager -shared -municode *.c ../GameManager/GameManager.dll -o MyChess.dll
