712eb1ef5af4821d127b4787f1f958b87d45b051
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2020-01-30 14:40:48 +0100

    Update Compiling.sh

--- GameManager/UciClient/Compiling.sh
+++ GameManager/UciClient/Compiling.sh
@@ -1,4 +1,4 @@
 set -x
 rm -f UciClient
-gcc -I../GameManager -D_LIB ../GameManager/*.c ../MyChess/*.c *.c -L. -o UciClient
+gcc -I../GameManager -D_LIB ../GameManager/*.c ../MyChess/*.c UciClient.c -L. -o UciClient
 strip UciClient
