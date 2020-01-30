43b29603d29cdb6e2d07745d2318e6faf92c6d46
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2020-01-30 17:02:31 +0100

    Update CrossCompiling.sh

--- GameManager/TestMyChess/CrossCompiling.sh
+++ GameManager/TestMyChess/CrossCompiling.sh
@@ -1,3 +1,4 @@
 set -x
 rm -f TestMyChess.exe
-x86_64-w64-mingw32-gcc -I../GameManager -D_LIB ../GameManager/*.c ../MyChess/*.c TestMyChess.c -static -g -L. -lpthread -o TestMyChess.exe
+x86_64-w64-mingw32-gcc -I../GameManager -D_LIB ../GameManager/*.c ../MyChess/*.c TestMyChess.c -static -L. -lpthread -o TestMyChess.exe
+strip TestMyChess.exe
