d3c9c0a3fed92851cc2b9f7bd01d2a3d75b76d90
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2020-01-30 13:40:04 +0100

    Update GameManager.h

--- GameManager/GameManager/GameManager.h
+++ GameManager/GameManager/GameManager.h
@@ -1,24 +1,13 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+	
+#include "Includes.h"
 
 #if !defined(_MSC_VER) && !defined(__MINGW32__)
-#undef FAR
-#define FAR
-#undef PASCAL
-#define PASCAL
-#undef WINAPI
-#define WINAPI
-#define	LPSTR char *
-#define LPVOID void *
-#define BOOL unsigned int
-#define DWORD unsigned int
 #define LPCWSTR const char *
-#define LPCSTR const char *
-#define HINSTANCE int
 #define HANDLE int
 #define INVALID_HANDLE_VALUE -1
-#define DLL_PROCESS_ATTACH 0
 #define INFINITE -1
 
 typedef struct {
@@ -33,36 +22,6 @@ enum { FALSE, TRUE };
 
 typedef	unsigned long long	U64;			// Tipo intero senza segno a 64 bit
 
-enum {
-    UNKNOWN_SCORE = -2140000000L,
-    LOSS_SCORE = -2130000000L,
-    DRAW_SCORE = 0,
-    WIN_SCORE = 2130000000L
-};
-
-
-typedef enum {
-    kKEEPSEARCHING = 0,
-    kSTOPSOON = 1,
-    kSTOPNOW = 2
-} Search_Status;
-
-
-typedef enum {
-    DLL_OK = 0,
-    DLL_OK_DONT_SEND_SETUP = 1, // only supported in 1.0.2 and higher!
-
-    DLL_GENERIC_ERROR = -1,
-    DLL_OUT_OF_MEMORY_ERROR = -2,
-    DLL_UNKNOWN_VARIANT_ERROR = -3,
-    DLL_UNKNOWN_PLAYER_ERROR = -4,
-    DLL_UNKNOWN_PIECE_ERROR = -5,
-    DLL_WRONG_SIDE_TO_MOVE_ERROR = -6,
-    DLL_INVALID_POSITION_ERROR = -7,
-    DLL_NO_MOVES = -8
-} DLL_Result;
-	
-	
 // Tipi di ricerca
 
 #define	STR_ALPHABETA	"AlphaBeta"
@@ -193,6 +152,17 @@ OURLIB_API	void TakeNullMove(void);
 OURLIB_API	int InitDll(void);
 OURLIB_API	int GetPvScore(void);
 OURLIB_API	void Uci_Loop(void);
+	
+typedef int             (*PEXTRADEPTH)(void);
+typedef void    (*PMAKENULLMOVE)(void);
+typedef void    (*PTAKENULLMOVE)(void);
+typedef int             (*PISDRAW)(void);
+typedef int             (*PCANDONULL)(void);
+typedef CompMove  (*PGETBOOKMOVE)(void);
+typedef void    (*PSETHISTORY)(CompMove move,int depth);
+typedef void    (*PSETKILLERS)(CompMove *move);
+typedef char*   (*PPRINTMOVE)(CompMove *move);
+typedef int             (*PGETPVSCORE)(void);
 
 #ifdef __cplusplus
 }
