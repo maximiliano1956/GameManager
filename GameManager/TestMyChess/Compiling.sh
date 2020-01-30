8005a4486e90c043052cb8b432da652d3977e6e2
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2020-01-30 15:06:15 +0100

    Update Compiling.sh

--- GameManager/TestMyChess/Compiling.sh
+++ GameManager/TestMyChess/Compiling.sh
@@ -1,3 +1,3 @@
 set -x
-rm -f TestChess
-gcc -I../GameManager -D_LIB ../GameManager/*.c ../MyChess/*.c TestChess.c -g -L. -lpthread -o TestChess
+rm -f TestMyChess
+gcc -I../GameManager -D_LIB ../GameManager/*.c ../MyChess/*.c TestChess.c -g -L. -lpthread -o TestMyChess
