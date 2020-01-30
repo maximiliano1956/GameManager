9b6af5b000fd7600276b8c8ce52fa6b6336c7826
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2020-01-30 13:12:23 +0100

    Update CrossCompilingDll.sh

--- GameManager/CrossCompilingDll.sh
+++ GameManager/CrossCompilingDll.sh
@@ -1,10 +1,10 @@
 set -x
 cd GameManager
 rm -f GameManager.dll
-i686-w64-mingw32-gcc -shared -municode *.c GameManager_dll.def -o GameManager.dll
+i686-w64-mingw32-gcc -shared -municode -DGAMEMANAGER_EXPORTS -DGAME_IMPORTS *.c GameManager_new.def -o GameManager.dll
 cd ../MyChess
 rm -f MyChess.dll
-i686-w64-mingw32-gcc -I../GameManager -shared -municode *.c ../GameManager/GameManager.dll -o MyChess.dll
+i686-w64-mingw32-gcc -I../GameManager -shared -municode -DGAMEMANAGER_IMPORTS -DGAME_EXPORTS *.c ../GameManager/GameManager.dll -o MyChess.dll
 cd ../MyTicTacToe
 rm -f MyTicTacToe.dll
-i686-w64-mingw32-gcc -I../GameManager -shared -municode *.c ../GameManager/GameManager.dll -o MyTicTacToe.dll
+i686-w64-mingw32-gcc -I../GameManager -shared -municode -DGAMEMANAGER_IMPORTS -DGAME_EXPORTS *.c ../GameManager/GameManager.dll -o MyTicTacToe.dll
