f98fecdeab89cb8c50c0fbf6a51bfd7c2c53b61a
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2020-01-31 12:19:30 +0100

    Update CrossCompiling.sh

--- GameManager/TestMyTicTacToe/CrossCompiling.sh
+++ GameManager/TestMyTicTacToe/CrossCompiling.sh
@@ -1,4 +1,4 @@
 set -x
 rm -f TestMyTicTacToe.exe
-x86_64-w64-mingw32-gcc -I../GameManager -D_LIB ../GameManager/*.c ../MyTicTacToe/*.c TestMyTicTacToe.c -L. -o TestMyTicTacToe.exe
+x86_64-w64-mingw32-gcc -I../GameManager -D_LIB ../GameManager/*.c ../MyTicTacToe/*.c TestMyTicTacToe.c -L. -lpthread -o TestMyTicTacToe.exe
 strip TestMyTicTacToe.exe
