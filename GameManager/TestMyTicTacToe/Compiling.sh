8e6d2ee9a8f4a800f745e51419a042efde7de390
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2020-01-31 12:18:45 +0100

    Update Compiling.sh

--- GameManager/TestMyTicTacToe/Compiling.sh
+++ GameManager/TestMyTicTacToe/Compiling.sh
@@ -1,4 +1,4 @@
 set -x
 rm -f TestMyTicTacToe
-gcc -I../GameManager -D_LIB ../GameManager/*.c ../MyTicTacToe/*.c TestMyTicTacToe.c -L. -o TestMyTicTacToe
+gcc -I../GameManager -D_LIB ../GameManager/*.c ../MyTicTacToe/*.c TestMyTicTacToe.c -L. -lpthread -o TestMyTicTacToe
 strip TestMyTicTacToe
