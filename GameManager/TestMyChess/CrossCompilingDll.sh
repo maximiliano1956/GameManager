b030475ff24af1ad80ac3bdd6f24580f7aadda25
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2020-02-07 17:43:21 +0100

    Update CrossCompilingDll.sh

--- GameManager/TestMyChess/CrossCompilingDll.sh
+++ GameManager/TestMyChess/CrossCompilingDll.sh
@@ -1,13 +1,11 @@
 set -x
 cd ../GameManager
-rm -f GameManager.dll
-i686-w64-mingw32-gcc -shared -municode -DGAMEMANAGER_EXPORTS -DGAME_IMPORTS *.c GameManager_dll.def -o GameManager.dll
+rm -f ../TestMyChess/GameManager.dll
+i686-w64-mingw32-gcc -shared -municode -DGAMEMANAGER_EXPORTS -DGAME_IMPORTS *.c GameManager_dll.def -o ../TestMyChess/GameManager.dll
 cd ../MyChess
-rm -f MyChess.dll
-i686-w64-mingw32-gcc -I../GameManager -shared -municode -DGAMEMANAGER_IMPORTS -DGAME_EXPORTS *.c ../GameManager/GameManager.dll -o MyChess.dll
+rm -f ../TestMyChess/MyChess.dll
+i686-w64-mingw32-gcc -I../GameManager -shared -municode -DGAMEMANAGER_IMPORTS -DGAME_EXPORTS *.c ../TestMyChess/GameManager.dll -o ../TestMyChess/MyChess.dll
 cd ../TestMyChess
 rm -f TestMyChessDll.exe
 i686-w64-mingw32-gcc -I../GameManager -mconsole TestMyChessDll.c -lpthread -static -o TestMyChessDll.exe
-cp ../GameManager/GameManager.dll .
-cp ../MyChess/MyChess.dll .
 strip TestMyChessDll.exe
