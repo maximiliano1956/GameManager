f68479fe924a5fd76a8fbea2a91bebfefb175251
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2020-10-21 12:37:56 +0200

    Update CrossCompilingDll.sh

--- GameManager/TestMyChess/CrossCompilingDll.sh
+++ GameManager/TestMyChess/CrossCompilingDll.sh
@@ -4,7 +4,7 @@ rm -f ../TestMyChess/GameManager.dll
 i686-w64-mingw32-gcc -shared -municode -DGAMEMANAGER_EXPORTS -DGAME_IMPORTS *.c GameManager_dll.def -o ../TestMyChess/GameManager.dll
 cd ../MyChess
 rm -f ../TestMyChess/MyChess.dll
-i686-w64-mingw32-gcc -I../GameManager -shared -municode -DGAMEMANAGER_IMPORTS -DGAME_EXPORTS *.c ../TestMyChess/GameManager.dll -o ../TestMyChess/MyChess.dll
+i686-w64-mingw32-gcc -I../GameManager -shared -municode -DGAMEMANAGER_IMPORTS -DGAME_EXPORTS *.c ./nnue/*.cpp ../TestMyChess/GameManager.dll -o ../TestMyChess/MyChess.dll
 cd ../TestMyChess
 rm -f TestMyChessDll.exe
 i686-w64-mingw32-gcc -I../GameManager -mconsole TestMyChessDll.c -lpthread -static -o TestMyChessDll.exe
